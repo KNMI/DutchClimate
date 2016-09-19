bcf1ee88b9ca7bf455409c50cdc210a3150c3bf3
MartinRoth <marsianus@gmail.com>
2016-09-19 13:12:52 +0200

    Skip KIS tests on travis

--- tests/testthat/test-KISdata.R
+++ tests/testthat/test-KISdata.R
@@ -1,6 +1,7 @@
 context("KIS data extraction")
 
 test_that("De Bilt temperature", {
+  skip_on_travis()
   expect_match(WriteKISRecipe('TG', '260_H', '2016'), 'KIStable.txt')
   expect_error(KIS('rr', '260_H', '2016'), "Must be element of set {'TG'}.", fixed = TRUE)
   expect_error(KIS('TG', '260_H', '2016'), "Not implemented")
