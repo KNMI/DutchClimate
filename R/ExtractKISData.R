63e91ef27008489c3ae1016733b4bcdf4ad58cb7
MartinRoth <marsianus@gmail.com>
2016-10-25 18:18:49 +0200

    First working template for data retrieval

--- R/ExtractKISData.R
+++ R/ExtractKISData.R
@@ -1,35 +1,58 @@
 #' Extract data from KIS
 #' @param var Variable to extract (plural?)
 #' @param geoIdentifier Station identifier (, spatial point, spatial area ..., plural?)
-#' @param period Xts
+#' @param period Either numeric, timeBased or ISO-8601 style (see \code{\link[xts]{.subset.xts}})
 #' @return data.table
 #' @export
 KIS <- function(var, geoIdentifier, period) {
   assertChoice(var, "TG")
-  assertChoice(geoIdentifier, "260_H")
-  assertChoice(period, '2016')
+  assertChoice(geoIdentifier, c("260_H", "310_H"))
+  tryCatch(xts::.parseISO8601(period),
+           warning = function(cond) {
+             stop("period does not seem to be suitable")
+           },
+           error = function(cond) {
+             stop("period does not seem to be suitable")
+           })
   recipeName <- WriteKISRecipe(var, geoIdentifier, period)
-  ExecuteKISRecipe(recipeName, period)
+  result <- ExecuteKISRecipe(recipeName, period)
+  return(result) #FIX: Timezone is 240000 this day or the next?
 }
 
 WriteKISRecipe <- function(var, geoIdentifier, period) {
+  # period is not yet used in the recipe
+  # max results does not seem to have
   recipeName <- "KIStable.txt"
-  recipe     <- 'recipe={"datasetserieselements":[{"datasetseries":"REH1","element":"TG","unit":"graad C"}],"datasetseriesnames":["REH1"],"datasourcecodes":["260_H"],"intervalids":[],"elementgroupnames":[],"unitsettings":[{"unit":"graad C","scale":"true","conversionfunction":"NONE"}],"starttime":"20160915_000000_000000","endtime":"20160916_000000_000000","maxresults":100,"countsettings":{"count":false,"period":"DAY","countconditionbyelement":[{"element":"TG","condition":"AMOUNT","value":null}]},"displaysettings":{"showMetaData":false,"sort":"DateStationTime"}}'
+  recipe     <- 'recipe='
+  recipe     <- paste0(recipe, '{"datasetserieselements":[{"datasetseries":"REH1","element":"TG","unit":"graad C"}],')
+  recipe     <- paste0(recipe, '"datasetseriesnames":["REH1"],')
+  recipe     <- paste0(recipe, '"datasourcecodes":["', geoIdentifier, '"],')
+  recipe     <- paste0(recipe, '"intervalids":[],"elementgroupnames":[],"unitsettings":[{"unit":"graad C","scale":"true","conversionfunction":"NONE"}],"starttime":"20160115_000000_000000","endtime":"20160916_000000_000000","maxresults":1000,"countsettings":{"count":false,"period":"DAY","countconditionbyelement":[{"element":"TG","condition":"AMOUNT","value":null}]},"displaysettings":{"showMetaData":false,"sort":"DateStationTime"}}')
+  recipe     <- str_replace_all(recipe, '%', '%25')
   writeLines(recipe, recipeName)
   return(recipeName)
 }
 
+CorrectDataFormat <- function(xtsObject) {
+  format(xtsObject, "%Y%m%d_%H%M%S")
+}
+
 ExecuteKISRecipe <- function(recipeName, period) {
-  url <- 'http://bhlkisdev.knmi.nl:8080/kis/kis/download/table/20160901_000001/20160919_000000/CSV'
+  parsedPeriod <- .parseISO8601(period)
+  url <- 'http://kisapp.knmi.nl:8080/servlet/download/table/'
+  url <- paste0(url, CorrectDataFormat(parsedPeriod$first.time+1),
+                '/', CorrectDataFormat(parsedPeriod$last.time+1), '/CSV')
   destFile <- 'KIStable.csv'
-  download.file(url, destFile, method = "wget",
+
+  flog.info("Start data download.")
+  download.file(url, destFile, method = "wget", quiet = T,
                 extra = c('--header="Content-Type:application/x-www-form-urlencoded"',
-                          paste0('--post-file="', recipeName, '"'),
-                          '-v'))
+                          paste0('--post-file="', recipeName, '"')))
+  flog.info("Download finished.")
 
   result <- tryCatch(fread(destFile),
                      warning = function(cond) {
-                       mesage("URL caused a warning")
+                       message("URL caused a warning")
                        return(NULL)
                      },
                      error = function(cond) {
@@ -42,4 +65,5 @@ ExecuteKISRecipe <- function(recipeName, period) {
                      })
   return(result)
 }
+
 
--- R/imports.R
+++ R/imports.R
@@ -1,3 +1,6 @@
 #' @import checkmate
 #' @import data.table
+#' @import futile.logger
+#' @importFrom xts .parseISO8601
+#' @importFrom stringr str_replace_all
 NULL
