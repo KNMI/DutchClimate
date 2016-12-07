15ed91ac261d97196e4fc0980b431873a721f720
MartinRoth <marsianus@gmail.com>
2016-12-07 14:19:08 +0100

    Small updates in the python wrapper

--- inst/python/import.py
+++ inst/python/import.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 ## python import data and plot
 
 import rpy2.robjects as rpy
@@ -8,15 +10,14 @@ import numpy as np
 pandas2ri.activate()
 dc = importr("DutchClimate")
 
-kisdata = pandas2ri.ri2py(dc.KIS("TG", '260_a', '2015'))
+kisdata = pandas2ri.ri2py(dc.KIS("TG", '260_H', '2015'))
 
 # aggregated = python.wrapp(dc.aggregate(kisdata))
 #
 # plotfile = python(dc.plotdata(aggregated))
 # rpy.r('hist(%s, xlab = "x", main = "hist(x)")' %kisdata.r_repr())
 
-# plt.hist(kisdata['var'])
-plt.hist(np.asarray(kisdata['var']))
+plt.hist(np.asarray(kisdata['REH1.TG']))
 plt.show()
 
 
